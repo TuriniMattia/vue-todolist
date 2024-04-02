25d6f300dca7ab1d03a29d4e31316cda93de367f
TuriniMattia <turinimattia96@gmail.com>
2024-04-02 22:24:01 +0200

    [add]completato base + bonus

---
+++ app.js
@@ -0,0 +1,48 @@
+
+
+const { createApp } = Vue
+
+const todoItems = [
+    {
+        text : 'devo fare i compiti',
+        done : true,
+    },
+    {
+        text : 'devo fare la spesa',
+        done : false,
+    },
+    {
+        text : 'devo pagare le bollette',
+        done : false,
+    },
+]
+
+createApp({
+  data() {
+    return {
+      todos: todoItems,
+      inputValue : ''
+    }
+  },
+  methods: {
+    deleteItem(index){
+        this.todos.splice(index, 1)
+    },
+    addTodo(){
+       
+
+        if(this.inputValue !== ''){
+            const item = {
+                text : this.inputValue,
+                done : false
+            }
+            this.todos.push(item);
+            this.inputValue = ''
+
+        }
+        
+    }
+  }
+}).mount('#app')
+
+
